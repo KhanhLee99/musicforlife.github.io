d16e71c8d75fdd72dc28eaf4523a0ee2e1751926
KhanhLe <levietkhanh99@gmail.com>
2021-09-08 23:46:34 +0700

    push

---
+++ chart/chart/Test/lientamtestjs.js
@@ -0,0 +1,26 @@
+var lineChartData = {
+			labels : ["January","February","March","April","May","June","July"],
+			datasets : [
+				{
+					label: "My First dataset",
+					fillColor : "rgba(220,220,220,0.2)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(220,220,220,1)",
+					 data:[ 100, 500, 600,300,50,900,400]
+				},
+				{
+					label: "My Second dataset",
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 1)",
+					pointColor : "rgba(48, 164, 255, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					 data:[ 700, 0, 400,800,50,700,200]
+				}
+			]
+
+		}
